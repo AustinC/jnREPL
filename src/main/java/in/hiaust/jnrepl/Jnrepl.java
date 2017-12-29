c3c8a3edd33991e584743c8764429373649877f6
Austin Chamberlin <austin.r.chamberlin@gmail.com>
2017-12-29 10:10:25 -0800

    implementation

--- src/main/java/in/hiaust/jnrepl/Jnrepl.java
+++ src/main/java/in/hiaust/jnrepl/Jnrepl.java
@@ -1,12 +1,46 @@
 package in.hiaust.jnrepl;
 
+import clojure.java.api.Clojure;
+import clojure.lang.IFn;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Jnrepl {
 
-    public static void startRepl() {
+    final static Logger logger = LoggerFactory.getLogger(Jnrepl.class);
 
+    private static Object serverInstance = null;
+
+    /**
+     * Start a clojure nREPL server
+     */
+    public static synchronized void startRepl() {
+
+        try {
+            IFn require = Clojure.var("clojure.core", "require");
+            require.invoke(Clojure.read("clojure.tools.nrepl.server"));
+
+            Long port = Long.parseLong(System.getProperty("jnrepl.port", "9090"));
+            IFn server = Clojure.var("clojure.tools.nrepl.server", "start-server");
+            serverInstance = server.invoke(Clojure.read(":port"), port);
+
+            logger.info("Started clojure nREPL on port %s", port);
+        }
+        catch (Throwable e) {
+            logger.error("Error starting nrepl", e);
+        }
     }
 
-    public static void shutdownRepl() {
-        
+    /**
+     * Shutdown the nREPL server
+     */
+    public static synchronized void shutdownRepl() {
+        if (null == serverInstance) {
+            return;
+        }
+
+        IFn shutdownFn = Clojure.var("clojure.tools.nrepl.server", "stop-server");
+        shutdownFn.invoke(serverInstance);
+        serverInstance = null;
     }
 }
